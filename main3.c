edc05f35ded11e6bc3bae94cf0c393befb55148c
APuehler <86292991+APuehler@users.noreply.github.com>
2023-12-14 12:32:23 +0100

    Update main3.c
    correcter

--- main3.c
+++ main3.c
@@ -1,17 +1,14 @@
 #include <pthread.h>
 #include <semaphore.h>
-#include <mqueue.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define CYCLE_TIME_MS 4
 #define PROCESS_TIME_TASK1_MS 2
 #define PROCESS_TIME_TASK2_MS 3
-#define MQ_NAME "/task_mq"
 
-// Macro für verbessertes Debugging
+// Makro für verbessertes Debugging
 #define CALLNEW(call) \
 do { \
     int ret = call; \
@@ -25,10 +22,8 @@ do { \
 void *task1(void *arg);
 void *task2(void *arg);
 void waste_msecs(unsigned int msecs);
-unsigned long long fibonacci(unsigned long long n);
 
 sem_t sem_task1;
-mqd_t mq;
 int count = 0;
 
 int main() {
@@ -36,14 +31,6 @@ int main() {
 
     // Initialisierung der Semaphore
     CALLNEW(sem_init(&sem_task1, 0, 0));
-
-    // Erstellung der Message Queue (falls erforderlich)
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = sizeof(int);
-    attr.mq_curmsgs = 0;
-    CALLNEW(mq_open(MQ_NAME, O_CREAT | O_RDWR, 0644, &attr) != (mqd_t)-1);
 
     // Erstellen der Threads
     CALLNEW(pthread_create(&t1, NULL, task1, NULL));
@@ -54,8 +41,6 @@ int main() {
     CALLNEW(pthread_join(t2, NULL));
 
     // Aufräumen
-    CALLNEW(mq_close(mq));
-    CALLNEW(mq_unlink(MQ_NAME));
     CALLNEW(sem_destroy(&sem_task1));
 
     return 0;
@@ -63,16 +48,13 @@ int main() {
 
 void *task1(void *arg) {
     while (1) {
-        // Verarbeitungszeit simulieren
+        // Simuliert die Verarbeitungszeit
         waste_msecs(PROCESS_TIME_TASK1_MS);
 
         // Semaphore setzen alle 3 Zyklen
         if (++count % 3 == 0) {
             CALLNEW(sem_post(&sem_task1));
         }
-
-        // Warten bis zum nächsten Zyklus
-        waste_msecs(CYCLE_TIME_MS - PROCESS_TIME_TASK1_MS);
     }
 }
 
@@ -96,8 +78,7 @@ unsigned long long fibonacci(unsigned long long n) {
 
 void waste_msecs(unsigned int msecs) {
     int elapsed_time = 0;
-
-    int duration = msecs * 180;
+    int duration = msecs * 180; // Anpassung der Dauer basierend auf der Verarbeitungszeit
 
     while (elapsed_time < duration) {
         fibonacci(10);
