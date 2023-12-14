c33edf90251bd7d8cb6a2532a58170d6d11e4b02
APuehler <86292991+APuehler@users.noreply.github.com>
2023-12-14 12:19:32 +0100

    Update main2..c
    updated

--- main2..c
+++ main2..c
@@ -1,9 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <mqueue.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
 
+#define CYCLE_TIME_MS 4
+#define PROCESS_TIME_TASK1_MS 2
+#define PROCESS_TIME_TASK2_MS 3
+#define MQ_NAME "/task_mq"
 
 #define CALLNEW(call) \
 do { \
@@ -23,60 +30,57 @@ unsigned long long fibonacci(unsigned long long n) {
 }
 
 void waste_msecs(unsigned int msecs) {
-    uint64_t nanos = (uint64_t)msecs * 1000000;
-    uint64_t elapsed_time = 0;
+    int elapsed_time = 0;
+    int duration = msecs * 180; 
 
-    while (elapsed_time < nanos) {
-        fibonacci(100);
-        elapsed_time += 1000000; // Simulate 1 ms of computation
+    while (elapsed_time < duration) {
+        fibonacci(10);
+        elapsed_time += 1;
     }
 }
 
-void *time_meassure(void *arg) {
-    // prio
-    pthread_attr_t attr;
-    CALLNEW(pthread_attr_init(&attr));
-    CALLNEW(pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED));
-    struct sched_param param;
-    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    CALLNEW(pthread_attr_setschedparam(&attr, &param));
+void *task1(void *arg) {
+    while (1) {
+        waste_msecs(PROCESS_TIME_TASK1_MS);
 
-    // 
-    int policy;
-    CALLNEW(pthread_attr_getschedpolicy(&attr, &policy));
-    CALLNEW(pthread_attr_getschedparam(&attr, &param));
-    printf("Main thread priority: %d (Scheduling Policy: %s)\n", param.sched_priority,
-           (policy == SCHED_FIFO) ? "SCHED_FIFO" : "SCHED_OTHER");
-
-    struct timespec start_time;
-    CALLNEW(clock_gettime(CLOCK_MONOTONIC, &start_time));
-
-    waste_msecs(1000);
-
-
-    struct timespec end_time;
-    CALLNEW(clock_gettime(CLOCK_MONOTONIC, &end_time));
-
-
-    uint64_t elapsed_nanos = ((uint64_t)end_time.tv_sec * 1000000000 + end_time.tv_nsec) -
-                             ((uint64_t)start_time.tv_sec * 1000000000 + start_time.tv_nsec);
-
-    printf("Elapsed time: %llu nanoseconds\n", (unsigned long long)elapsed_nanos);
-
-    return NULL;
+        if (++count % 3 == 0) {
+            CALLNEW(sem_post(&sem_task1));
+        }
+    }
 }
 
+void *task2(void *arg) {
+    while (1) {
+        CALLNEW(sem_wait(&sem_task1));
+        waste_msecs(PROCESS_TIME_TASK2_MS);
+    }
+}
+
+sem_t sem_task1;
+mqd_t mq;
+int count = 0;
+
 int main() {
-    pthread_t main_thread;
-    CALLNEW(pthread_create(&main_thread, NULL, time_meassure, NULL));
+    pthread_t t1, t2;
 
+    CALLNEW(sem_init(&sem_task1, 0, 0));
 
-    struct sched_param main_param;
-    main_param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    CALLNEW(pthread_setschedparam(pthread_self(), SCHED_FIFO, &main_param));
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(int);
+    attr.mq_curmsgs = 0;
+    CALLNEW(mq_open(MQ_NAME, O_CREAT | O_RDWR, 0644, &attr) != (mqd_t)-1);
 
+    CALLNEW(pthread_create(&t1, NULL, task1, NULL));
+    CALLNEW(pthread_create(&t2, NULL, task2, NULL));
 
-    CALLNEW(pthread_join(main_thread, NULL));
+    CALLNEW(pthread_join(t1, NULL));
+    CALLNEW(pthread_join(t2, NULL));
+
+    CALLNEW(mq_close(mq));
+    CALLNEW(mq_unlink(MQ_NAME));
+    CALLNEW(sem_destroy(&sem_task1));
 
     return 0;
 }
